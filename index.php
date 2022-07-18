34cebedbffd0a9ffae54b1eeb64fb7523739d264
priya8425 <yadavpriya1425@gmail.com>
2022-07-18 18:33:16 +0530

    email

--- index.php
+++ index.php
@@ -300,22 +300,22 @@ Getting Genuine Service from Aayushi Water Purifier is important as only from Aa
           <div class="row">
             <div class="col-md-4 form-group">
               <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-              <div class="validate"></div>
+              
             </div>
             <div class="col-md-4 form-group mt-3 mt-md-0">
               <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email">
-              <div class="validate"></div>
+              
             </div>
             <div class="col-md-4 form-group mt-3 mt-md-0">
               <input type="tel" class="form-control" name="mobile" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-              <div class="validate"></div>
+              
             </div>
           </div>
           
 
           <div class="form-group mt-3">
             <textarea class="form-control" name="message" rows="5" placeholder="Message (Optional)"></textarea>
-            <div class="validate"></div>
+           
           </div>
           <div class="mb-3">
             <div class="loading">Loading</div>
