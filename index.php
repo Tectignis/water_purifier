cbc68ed769093c1b478f403a71e1a6e0fcf0dd17
priya8425 <yadavpriya1425@gmail.com>
2022-07-18 18:32:13 +0530

    mail

--- index.php
+++ index.php
@@ -296,7 +296,7 @@ Getting Genuine Service from Aayushi Water Purifier is important as only from Aa
           <p>Protect Your Family With The Next-Gen Aayushi Water Purifier Services</p>
         </div>
 
-        <form action="forms/appointment.php" method="post" role="form" class="php-email-form">
+        <form  method="post"  class="php-email-form">
           <div class="row">
             <div class="col-md-4 form-group">
               <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
@@ -307,7 +307,7 @@ Getting Genuine Service from Aayushi Water Purifier is important as only from Aa
               <div class="validate"></div>
             </div>
             <div class="col-md-4 form-group mt-3 mt-md-0">
-              <input type="tel" class="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+              <input type="tel" class="form-control" name="mobile" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
               <div class="validate"></div>
             </div>
           </div>
@@ -322,7 +322,7 @@ Getting Genuine Service from Aayushi Water Purifier is important as only from Aa
             <div class="error-message"></div>
             <div class="sent-message">Your  request has been sent successfully. Thank you!</div>
           </div>
-          <div class="text-center"><button type="submit">Book A Free Demo</button></div>
+          <div class="text-center"><button type="submit" name="submit">Book A Free Demo</button></div>
         </form>
 
       </div>
