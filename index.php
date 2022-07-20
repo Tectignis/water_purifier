558916e7d8bfd55dda3a2fdaeac69e06568e5045
priya8425 <yadavpriya1425@gmail.com>
2022-07-20 11:32:52 +0530

    logo image

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ if(isset($_POST['submit'])){
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index"><img src="assets/img/logo.png" alt="logo" height="250%" width="154%"></a></h1>
+      <h1 class="logo me-auto"><a href="index"><img src="assets/img/websitelogo.jpg" alt="logo" height="250%" width="154%"></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
